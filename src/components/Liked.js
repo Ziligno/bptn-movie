933d7252dd2b874c247fa7c13f94e8b7fbed9b3f
Ziligno <syamalalamichael@gmail.com>
2022-06-29 16:21:00 +0200

    adding navigation

---
+++ src/components/Liked.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import "../App.css";
+import axios from "axios";
+
+
+
+function Liked() { 
+
+    return (
+        <div>
+            <h2>Liked Movies</h2>
+        </div>
+    )
+}
+
+export default Liked
